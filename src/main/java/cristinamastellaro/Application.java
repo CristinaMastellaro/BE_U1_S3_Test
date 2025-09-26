f25393a427833a319206713340c5248d3e776834
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-09-26 10:16:10 +0200

    screen

--- src/main/java/cristinamastellaro/Application.java
+++ src/main/java/cristinamastellaro/Application.java
@@ -1,8 +1,14 @@
 package cristinamastellaro;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbl");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
     }
 }
